18fd1845013b75504c4351a234b0cd6d3505c34a
Tobinator2000 <steinlintobias@gmail.com>
2024-09-13 18:32:43 +0200

    Inventory and HUD Gui

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -4,10 +4,34 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private Slider healthSlider;
+    [SerializeField] private GameObject inventory;
+    bool isInventoryActive;
+
+    void Start()
+    {
+        inventory.SetActive(false);
+        isInventoryActive = false;
+    }
 
     public void UpdateHealth(float healthPercent)
     {
         // Assuming healthPercent is a value between 0 and 1 (normalized)
         healthSlider.value = healthPercent;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab) && !isInventoryActive)
+        {
+            inventory.SetActive(true);
+            isInventoryActive = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else if (Input.GetKeyDown(KeyCode.Tab) && isInventoryActive)
+        {
+            inventory.SetActive(false);
+            isInventoryActive = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
     }
 }
